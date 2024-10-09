207ef61157fadb483eae5ecf57876898198ae95b
JimyDickson <jimytavares@hotmail.com>
2024-10-09 10:03:58 -0300

    lgpd

--- resources/views/pages/lgpd.blade.php
+++ resources/views/pages/lgpd.blade.php
@@ -15,31 +15,29 @@
                 </div><hr><br/>
             </div>
 
-            <h4><b>Lei Geral de Proteção de Dados Pessoais</b></h4>
-
-            <div class="" style="margin-top:35px;">
-                <p style="font-size:17px;"><b>Horário de atendimento:</b> 8h às 14h, de segunda a sexta.<br/>
-                    <b>Endereço:</b> Centro de Convenções de Natal - Via Costeira Sen. Dinarte Medeiros Mariz, S/n - Ponta Negra, Natal/RN - CEP 59.090-002 CNPJ: 01.278.765/0001-50</p>
-            </div>
-
-            <h4 style="margin-top:35px;">Encarregados dos Dados Pessoais</h4>
+            <h4 style="margin-top:15px;">Encarregados e dos Dados Pessoais</h4>
 
             <h4 style="margin-top:30px;">Titular</h4>
 
-            <div class="" style="margin-top:35px;">
-                <p style="font-size:17px;"><b><i class="far fa-user"></i> LARISSA ANTONIA FRANÇA DE OLIVEIRA</b><br/>
-                Cargo: <br/>
-                E-mail: lgpd@setur.rn.gov.br<br/>
-                Telefone: (84) 99431-4887</p>
+            <div class="" style="margin-top:15px;">
+                <p style="font-size:17px;"><i class="far fa-user"></i> LARISSA ANTONIA FRANÇA DE OLIVEIRA<br/>
+                Cargo: <br/></p>
             </div>
 
             <h4 style="margin-top:30px;">Suplente</h4>
 
-            <div class="" style="margin-top:35px;">
-                <p style="font-size:17px;"><b><i class="far fa-user"></i> ANA IRIS ALVES DA SILVA</b><br/>
-                    Cargo: <br/>
-                    E-mail: lgpd@setur.rn.gov.br<br/>
-                    Telefone: (84) 99654-4192</p>
+            <div class="" style="margin-top:15px;">
+                <p style="font-size:17px;"><i class="far fa-user"></i> ANA IRIS ALVES DA SILVA<br/>
+                    Cargo: <br/></p>
+            </div>
+            
+            <br/>
+            <h4><b>Localização</b></h4>
+
+            <div class="" style="margin-top:15px;">
+                <p>Centro de Convenções de Natal - Via Costeira Sen. Dinarte Medeiros Mariz, S/n <br/> Ponta Negra, Natal/RN <br/> CEP 59.090-002 <br/> CNPJ: 01.278.765/0001-50</p>
+                <p style="font-size:17px;"><b>E-Mail:</b> lgpd@setur.rn.gov.br<br/>
+                <b>Horário de atendimento:</b> 8h às 14h, de segunda a sexta.<br/>
             </div>
            
         </div>
